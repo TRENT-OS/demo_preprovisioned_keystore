3b5e1ff69885c9d0e929256cc83a8000c2ed1a37
Felix Schladt <felix.schladt@hensoldt.net>
2024-03-26 11:40:54 +0100

    [DOC] Update Copyright
    Change files to GPLv2 or Commercial licensing

--- components/ChanMux/src/instance/ChanMux_Instance.c
+++ components/ChanMux/src/instance/ChanMux_Instance.c
@@ -1,7 +1,11 @@
 /*
- *  WAN/LAN/NetworkStack Channel MUX
+ * WAN/LAN/NetworkStack Channel MUX
  *
- *  Copyright (C) 2019, Hensoldt Cyber GmbH
+ * Copyright (C) 2019-2024, HENSOLDT Cyber GmbH
+ * 
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * For commercial licensing, contact: info.cyber@hensoldt.net
  */
 
 #include "ChanMux/ChanMux.h"
